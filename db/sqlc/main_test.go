8c79ca498642cbbdf338ca00cc54dad098522aff
Albert Halim <albertcristiantohalim@mail.ugm.ac.id>
2026-01-30 19:22:53 +0700

    feat: first commit

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDB *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error  // Declare err first
+	testDB, err = sql.Open(dbDriver, dbSource)  // Use = not :=
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	
+	testQueries = New(testDB)
+
+	os.Exit(m.Run())
+}
